0acf1a5bf2d4d0c849f1fcdcd4620066be3dcdc1
2hanhan <1085771291@qq.com>
2022-04-26 17:53:09 +0800

    name sort

--- get_all_file_name.py
+++ get_all_file_name.py
@@ -6,13 +6,19 @@ fw_rel = open(relativepath, "w")
 fw_abs = open(absolutepath, "w")
 
 
+def name_sort(files):#文件名排序 
+    files_list = sorted(files)
+    return files_list
+
+
 def get_all_file_name(dir):
     for root, dirs, files in os.walk(dir):
-        dir
         # print("根目录", root)  # 当前目录路径
         # print("子目录", dirs)  # 当前路径下所有子目录
         # print("文件名", files)  # 当前路径下所有非目录子文件
-        for file in files:
+        files_list = name_sort(files)
+
+        for file in files_list:
             #print("路径", root)
             #print("文件名", file)
             rel = root.replace(dir, "", 1)
@@ -30,5 +36,6 @@ def get_all_file_name(dir):
 if __name__ == '__main__':
     root_dir = os.getcwd()  # 输入当前文件路径
     get_all_file_name(root_dir)
+    
     # dirpath = ""  # 指定根目录
     # read_directory(dirpath)
